8df1eb146044493beaa7a6e713274dcfa9960b8b
Isaia Riva <isaia.riva@finconsgroup.com>
2023-11-22 22:31:30 +0100

    project start up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Ciao Iso Bum</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
